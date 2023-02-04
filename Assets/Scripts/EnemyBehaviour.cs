03a6945d49ff7535b97954d5e647cda9abdbc805
LetsBigSnack <cc221019@fhstp.ac.at>
2023-02-04 11:06:49 +0100

    Added Enemy Splitting

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -9,9 +9,15 @@ public class EnemyBehaviour : MonoBehaviour
     private Rigidbody2D _enemyRB;
     private GameObject _player;
     [SerializeField] private float _movementSpeed = 3;
+    [SerializeField] private GameObject spawnedEnemy;
+    [SerializeField] private int invincibilityFrame = 15;
+    [SerializeField] private int currentinvincibilityFrame = 0;
+    [SerializeField] private bool isInvincible = true;
+
     // Start is called before the first frame update
     void Start()
     {
+        
         _player = FindObjectOfType<PlayerMovement>().gameObject;
         _enemyRB = GetComponent<Rigidbody2D>();
     }
@@ -28,12 +34,35 @@ public class EnemyBehaviour : MonoBehaviour
         
     }
 
-    
+    private void FixedUpdate()
+    {
+        if (currentinvincibilityFrame >= invincibilityFrame && isInvincible)
+        {
+            isInvincible = false;
+        }else if (currentinvincibilityFrame < invincibilityFrame)
+        {
+            currentinvincibilityFrame++;
+        }
+    }
+
+
     private void OnTriggerEnter2D(Collider2D col)
     {
         Debug.Log(col.gameObject.tag);
-        if (col.gameObject.tag == "sword")
+        if (col.gameObject.tag == "sword" && !isInvincible)
         {
+            if (spawnedEnemy)
+            {
+                
+                float x_Direction =  _player.transform.position.x - transform.position.x;
+                float y_Direction = _player.transform.position.y - transform.position.y;
+                double hypotenuse = Math.Sqrt(x_Direction * x_Direction + y_Direction * y_Direction);
+                float x_Direction_normalized = (float)(x_Direction / hypotenuse);
+                float y_Direction_normalized = (float)(y_Direction / hypotenuse);
+                
+                GameObject newEnemy1 = Instantiate(spawnedEnemy, new Vector3(transform.position.x-x_Direction_normalized*2, transform.position.y, 0), Quaternion.identity);
+                GameObject newEnemy2 = Instantiate(spawnedEnemy, new Vector3(transform.position.x, transform.position.y-y_Direction_normalized*2, 0), Quaternion.identity);
+            }
             Debug.Log("HIT");
             Destroy(this.gameObject);
         }
