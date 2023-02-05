56b62942f2c1fb418dec8e4e1b29b70c41cb3daf
LetsBigSnack <cc221019@fhstp.ac.at>
2023-02-05 08:17:20 +0100

    Added Enemy Prefabs

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -7,12 +7,10 @@ using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour
 {
     [SerializeField] string _sceneName;
-    [SerializeField] private Counter _counter;
 
     // Start is called before the first frame update
     void Start()
     {
-        _counter = FindObjectOfType<Counter>();
     }
 
     // Update is called once per frame
@@ -23,7 +21,7 @@ public class ChangeScene : MonoBehaviour
 
     public void changeScene()
     {
-        _counter.saveTime();
+       
         SceneManager.LoadScene(_sceneName);
     }
 }
