d8c484ed61565742b438d829891cb7fef8c6175c
LetsBigSnack <cc221019@fhstp.ac.at>
2023-02-05 11:58:06 +0100

    Added Player Follow to the XP

--- Assets/FollowPlayer.cs
+++ Assets/FollowPlayer.cs
@@ -25,15 +25,15 @@ public class FollowPlayer : MonoBehaviour
     {
         if (searchingPlayer)
         {
-           /**
-            *  float x_Direction =  _player.transform.position.x - transform.position.x;
+           
+            float x_Direction =  _player.transform.position.x - transform.position.x;
             float y_Direction = _player.transform.position.y - transform.position.y;
             double hypotenuse = Math.Sqrt(x_Direction * x_Direction + y_Direction * y_Direction);
             float x_Direction_normalized = (float)(x_Direction / hypotenuse);
             float y_Direction_normalized = (float)(y_Direction / hypotenuse);
-            transform.position += new Vector3( x_Direction_normalized * 1 * Time.deltaTime, 
-                transform.position.x + y_Direction_normalized * 1 * Time.deltaTime, 0);
-            */
+            transform.position += new Vector3( x_Direction_normalized , 
+                y_Direction_normalized , 0) * Time.deltaTime;
+            
         }
         
     }
